8d33e916c8310cd0289282ae0234bb61f10c6f0b
Chiel Kunkels <chiel@kunkels.me>
2022-10-09 16:02:53 +0200

    add basic pet frame

---
+++ unitframes/units/pet.lua
@@ -0,0 +1,24 @@
+local addon = select(2, ...)
+
+addon.units.pet = {
+	spawn = function(self)
+		local f = self:Spawn('pet', 'AelUIPet')
+		f:SetSize(150, 20)
+		f:SetPoint('BOTTOMRIGHT', AelUIPlayer, 'BOTTOMLEFT', -4, 0)
+	end,
+
+	style = function(self, unit)
+		self:RegisterForClicks 'AnyUp'
+		self:SetScript('OnEnter', UnitFrame_OnEnter)
+		self:SetScript('OnLeave', UnitFrame_OnLeave)
+
+		self.colors = addon.colors
+
+		local health = addon.elements.Health(self, unit)
+		health:SetAllPoints()
+
+		local name = addon.elements.Text(self.Health)
+		name:SetPoint('BOTTOMLEFT', self, 'TOPLEFT', 4, -6)
+		self:Tag(name, '[AelUI:name]')
+	end,
+}
