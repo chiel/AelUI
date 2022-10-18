ca48a2a340e4159827db189e5c57b002423bd89d
Chiel Kunkels <chiel@kunkels.me>
2022-10-18 12:17:12 +0200

    scale and move keystone frame

---
+++ blizzard/keystones.lua
@@ -0,0 +1,16 @@
+local addon = select(2, ...)
+
+local function ScaleKeystoneFrame()
+	local ckf = ChallengesKeystoneFrame
+	ckf:SetScale(0.75)
+	ckf:ClearAllPoints()
+	ckf:SetPoint('BOTTOM', addon.uiAnchor, 'TOP', 0, 120)
+end
+
+local f = CreateFrame 'Frame'
+f:SetScript('OnEvent', function(self, event, name)
+	if name == 'Blizzard_ChallengesUI' then
+		ChallengesKeystoneFrame:HookScript('OnShow', ScaleKeystoneFrame)
+	end
+end)
+f:RegisterEvent 'ADDON_LOADED'
