648391e38c2f3b992a406eb03c1c7d5a7f67bc09
Chiel Kunkels <chiel@kunkels.me>
2022-10-26 23:51:57 +0200

    add robotocondensed bold font

--- media.lua
+++ media.lua
@@ -3,6 +3,7 @@ local addon = select(2, ...)
 addon.media = {
 	border = [[Interface\Buttons\WHITE8X8]],
 	font = [[Interface\AddOns\AelUI\media\RobotoCondensed-Regular.ttf]],
+	fontBold = [[Interface\AddOns\AelUI\media\RobotoCondensed-Bold.ttf]],
 	fontPixel = [[Interface\AddOns\AelUI\media\semplice.ttf]],
 	texture = [[Interface\AddOns\AelUI\media\statusbar]],
 	textureBg = [[Interface\Buttons\WHITE8X8]],
@@ -10,8 +11,9 @@ addon.media = {
 
 local LSM = LibStub 'LibSharedMedia-3.0'
 local MT = LSM.MediaType
-LSM:Register(MT.FONT, 'AelUI font', addon.media.font)
-LSM:Register(MT.FONT, 'AelUI pixel font', addon.media.fontPixel)
+LSM:Register(MT.FONT, 'AelUI', addon.media.font)
+LSM:Register(MT.FONT, 'AelUI Bold', addon.media.fontBold)
+LSM:Register(MT.FONT, 'AelUI Pixel', addon.media.fontPixel)
 LSM:Register(MT.BACKGROUND, 'AelUI background', addon.media.textureBg)
 LSM:Register(MT.BORDER, 'AelUI border', addon.media.border)
 LSM:Register(MT.STATUSBAR, 'AelUI bar', addon.media.texture)
