bdaaad75d95996c017184aafba723b4eeb003624
Chiel Kunkels <chiel@kunkels.me>
2023-01-30 00:53:11 +0100

    change health text when dead

--- unitframes/elements/tags.lua
+++ unitframes/elements/tags.lua
@@ -2,17 +2,26 @@ local addon = select(2, ...)
 
 oUF.Tags.Events['AelUI:currenthealth'] = 'UNIT_HEALTH UNIT_MAXHEALTH'
 oUF.Tags.Methods['AelUI:currenthealth'] = function(unit)
-	return AbbreviateNumbers(UnitHealth(unit))
+	local hp = UnitHealth(unit)
+	if hp == 0 then
+		return ''
+	end
+	return AbbreviateNumbers(hp)
 end
 
 oUF.Tags.Events['AelUI:healthpercent'] = 'UNIT_HEALTH UNIT_MAXHEALTH'
 oUF.Tags.Methods['AelUI:healthpercent'] = function(unit)
+	local hp = UnitHealth(unit)
+	if hp == 0 then
+		return 'DEAD'
+	end
+
 	local max = UnitHealthMax(unit)
 	if max == 0 then
 		return 0
 	end
 
-	return string.format('%d%%', math.floor(UnitHealth(unit) / max * 100 + 0.5))
+	return string.format('%d%%', math.floor(hp / max * 100 + 0.5))
 end
 
 oUF.Tags.Events['AelUI:name'] = 'UNIT_NAME_UPDATE'
