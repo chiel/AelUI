3d6dc89bc3bc76680b3fc0522b0404819f39d618
Chiel Kunkels <chiel@kunkels.me>
2023-06-09 20:05:59 +0200

    add support for bold fonts in text element

--- AelUI/unitframes/elements/text.lua
+++ AelUI/unitframes/elements/text.lua
@@ -2,11 +2,22 @@ local addon = select(2, ...)
 
 function addon.elements.Text(parent, options)
 	local options = options or {}
-	options.pixel = options.pixel or false
 	options.size = options.size or 20
 
-	local font = options.pixel and addon.media.fontPixel or addon.media.font
-	local flags = options.pixel and 'MONOCHROME,OUTLINE' or 'OUTLINE'
+	local style = options.style or 'normal'
+	if options.pixel then
+		style = 'pixel'
+	end
+
+	local font = addon.media.font
+	if style == 'bold' then
+		font = addon.media.fontBold
+	end
+	if style == 'pixel' then
+		font = addon.media.fontPixel
+	end
+
+	local flags = style == 'pixel' and 'MONOCHROME,OUTLINE' or 'OUTLINE'
 
 	local text = parent:CreateFontString(nil, 'OVERLAY', 'GameFontNormal')
 	text:SetFont(font, options.size, flags)
