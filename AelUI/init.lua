d8bcf0f9b8b9cde98a329da3fedc7ad6bbfbcade
Chiel Kunkels <chiel@kunkels.me>
2023-06-15 02:26:09 +0200

    move location of slash command

--- AelUI/init.lua
+++ AelUI/init.lua
@@ -1,3 +1,18 @@
 local addon = select(2, ...)
 
 AelUI = {}
+
+SLASH_AELUI1 = '/aelui'
+SlashCmdList.AELUI = function(msg)
+	local cmd, opts = string.match(msg, '(%S+)')
+	cmd = string.lower(cmd or '')
+
+	if cmd == '' then
+		print 'show config'
+	elseif cmd == 'import' then
+		addon.weakauras.import()
+	else
+		print 'USAGE:'
+		print '- /aelui import - to import class-specific WeakAuras'
+	end
+end
--- AelUI/weakauras/init.lua
+++ AelUI/weakauras/init.lua
@@ -6,7 +6,7 @@ addon.weakauras = {}
 local playerClass = select(2, UnitClass 'player')
 playerClass = string.lower(playerClass)
 
-local import = function()
+addon.weakauras.import = function()
 	local classData = addon.weakauras.classes[playerClass]
 	if not classData then
 		print '[AelUI] No WeakAuras available for your class - sorry!'
@@ -26,22 +26,5 @@ local import = function()
 
 	if not ok and error ~= nil then
 		print('[AelUI] Error occured while trying to import WeakAuras', error)
-	end
-end
-
-SLASH_AELUI1 = '/aelui'
-SlashCmdList.AELUI = function(msg)
-	if not WeakAuras or not WeakAuras.Import then
-		return
-	end
-
-	local cmd, opts = string.match(msg, '(%S+)')
-	cmd = string.lower(cmd or '')
-
-	if cmd == 'import' then
-		import()
-	else
-		print 'USAGE:'
-		print '- /aelui import - to import class-specific WeakAuras'
 	end
 end
