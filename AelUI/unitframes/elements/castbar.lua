db701819a03fc035841f9e30930fa64f89b2c22f
Chiel Kunkels <chiel@kunkels.me>
2023-07-22 13:41:57 +0200

    castbar: updates

--- AelUI/unitframes/elements/castbar.lua
+++ AelUI/unitframes/elements/castbar.lua
@@ -1,18 +1,27 @@
 local addon = select(2, ...)
 
+local function CreatePip(element)
+	local frame = CreateFrame('Frame', nil, element, 'BackdropTemplate')
+	frame:SetBackdrop { edgeFile = addon.media.border, edgeSize = 1 }
+	frame:SetWidth(2)
+	frame:SetBackdropBorderColor(1, 1, 1)
+	return frame
+end
+
 function addon.elements.Castbar(self, unit)
 	local castbar = CreateFrame('StatusBar', nil, self)
 	castbar:SetStatusBarTexture(addon.media.texture)
-	castbar:SetStatusBarColor(22 / 255, 142 / 255, 198 / 255)
+	castbar:SetStatusBarColor(32 / 255, 32 / 255, 32 / 255)
 
 	local backdrop = addon.elements.Backdrop(castbar)
 	backdrop:SetFrameLevel(castbar:GetFrameLevel() - 1)
 	castbar:SetPoint('TOPLEFT', backdrop, 'TOPLEFT', 1, -1)
 	castbar:SetPoint('BOTTOMRIGHT', backdrop, 'BOTTOMRIGHT', -1, 1)
 
-	local Icon = castbar:CreateTexture(nil, 'OVERLAY')
-	Icon:SetPoint('BOTTOMRIGHT', backdrop, 'BOTTOMLEFT', -4, 0)
-	Icon:SetSize(32, 32)
+	local Spark = castbar:CreateTexture(nil, 'OVERLAY')
+	Spark:SetSize(10, 10)
+	Spark:SetBlendMode 'ADD'
+	Spark:SetPoint('CENTER', castbar:GetStatusBarTexture(), 'RIGHT', 0, 0)
 
 	local options = { pixel = true, size = 8 }
 
@@ -23,15 +32,17 @@ function addon.elements.Castbar(self, unit)
 	Time:SetPoint('TOPRIGHT', backdrop, 'BOTTOMRIGHT', -4, -6)
 	Time:SetJustifyH 'RIGHT'
 
-	backdrop.Icon = Icon
+	backdrop.Spark = Spark
 	backdrop.Text = Text
 	backdrop.Time = Time
 
-	castbar.Icon = Icon
+	castbar.CreatePip = CreatePip
+	castbar.Spark = Spark
 	castbar.Text = Text
 	castbar.Time = Time
 
 	self.Castbar = castbar
+	self.CastbarBackdrop = backdrop
 
 	return backdrop
 end
