9f3e36d039deebdd92bbf262c3a7bbfab35fa21f
Chiel Kunkels <chiel@kunkels.me>
2024-05-06 16:56:11 +0200

    utils: move addBackdrop util

--- AelUI/core/utils/init.lua
+++ AelUI/core/utils/init.lua
@@ -1,13 +1,3 @@
 local ns = select(2, ...)
 
 ns.utils = {}
-
-local m = ns.media
-
-ns.utils.AddBackdrop = function(frame)
-	local bg = frame:CreateTexture(nil, 'BORDER')
-	bg:SetTexture(m.backgrounds.default.file)
-	bg:SetVertexColor(50 / 255, 50 / 255, 50 / 255)
-	bg:SetPoint('TOPLEFT', 0, 0)
-	bg:SetPoint('BOTTOMRIGHT', 0, 0)
-end
