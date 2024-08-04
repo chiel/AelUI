500908a54ba4ce036aa315c89f7042dcfe31bc3d
Chiel Kunkels <chiel@kunkels.me>
2024-08-04 14:53:07 +0200

    feat: add OnInitialise and OnEnable handlers

--- AelUI/core/init.lua
+++ AelUI/core/init.lua
@@ -3,4 +3,29 @@ local addonName, ns = ...
 local addon = LibStub('AceAddon-3.0'):NewAddon(addonName)
 LibStub('AceEvent-3.0'):Embed(addon)
 
+ns.handlers = {
+	initialise = {},
+	enable = {},
+}
+
+ns.OnInitialise = function(fn)
+	table.insert(ns.handlers.initialise, fn)
+end
+
+ns.OnEnable = function(fn)
+	table.insert(ns.handlers.enable, fn)
+end
+
+function addon:OnInitialize()
+	for _, fn in ipairs(ns.handlers.initialise) do
+		fn()
+	end
+end
+
+function addon:OnEnable()
+	for _, fn in ipairs(ns.handlers.enable) do
+		fn()
+	end
+end
+
 ns.addon = addon
