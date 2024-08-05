97653d25eaee7c49c0076e746b7a33fe390754fd
Chiel Kunkels <chiel@kunkels.me>
2024-08-05 21:58:32 +0200

    feat: scaffold out weakauras class data

--- AelUI/retail/weakauras/main.lua
+++ AelUI/retail/weakauras/main.lua
@@ -14,5 +14,28 @@ local config = {
 }
 
 ns.OnEnable(function()
+	local classKeys = {}
+	for key, _ in pairs(ns.weakauras.data.classes) do
+		table.insert(classKeys, key)
+	end
+	table.sort(classKeys)
+
+	for i, key in ipairs(classKeys) do
+		local classData = ns.weakauras.data.classes[key]
+
+		config.args[key] = {
+			order = i,
+			type = 'execute',
+			name = classData.name,
+			desc = 'Import ' .. classData.name .. ' WeakAuras',
+			image = classData.icon,
+			imageWidth = 48,
+			imageHeight = 48,
+			func = function()
+				print('import', classData.name)
+			end,
+		}
+	end
+
 	ns.options.addSection('weakauras', config)
 end)
