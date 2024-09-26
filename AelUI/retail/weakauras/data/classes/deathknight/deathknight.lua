3e0018435692651a01da0be5e0c41927e2afe37d
Chiel Kunkels <chiel@kunkels.me>
2024-09-26 22:51:47 +0200

    wip deathknight

---
+++ AelUI/retail/weakauras/data/classes/deathknight/deathknight.lua
@@ -0,0 +1,57 @@
+local blood = {
+	baseline = {
+		class = {
+			spellIcon(43265), -- Death Coil
+		},
+		blood = {
+			spellIcon(195292), -- Death's Caress
+		},
+	},
+	talents = {
+		class = {},
+		spec = {
+			spellIcon(206931), -- Blooddrinker
+			spellIcon(50842), -- Blood Boil
+			spellIcon(221699), -- Blood Tap
+			spellIcon(194844), -- Bonestorm
+			spellIcon(274156), -- Consumption
+			spellIcon(49028), -- Dancing Rune Weapon
+			spellIcon(108199), -- Gorefiend's Grasp
+			spellIcon(206930), -- Heart Strike
+			spellIcon(206940), -- Mark of Blood
+			spellIcon(195182), -- Marrowrend
+			spellIcon(194679), -- Rune Tap
+			spellIcon(219809), -- Tombstone
+			spellIcon(55233), -- Vampiric Blood
+		},
+	},
+}
+
+local unholy = {
+	baseline = {
+		class = {
+			spellIcon(47541), -- Death Coil
+		},
+		unholy = {
+			spellIcon(207317), -- Epidemic
+			spellIcon(77575), -- Outbreak
+		},
+	},
+	talents = {
+		class = {},
+		spec = {
+			spellIcon(85948), -- Festering Strike
+			spellIcon(55090), -- Scourge Strike
+			spellIcon(46584), -- Raise Dead
+			spellIcon(63560), -- Dark Transformation
+			spellIcon(152280), -- Defile
+			spellIcon(275699), -- Apocalypse
+			spellIcon(207311), -- Clawing Shadows
+			spellIcon(390279), -- Vile Contagion
+			spellIcon(42650), -- Army of the Dead
+			spellIcon(49206), -- Summon Gargoyle
+			spellIcon(455395), -- Raise Abomination
+			spellIcon(207289), -- Unholy Assault
+		},
+	},
+}
