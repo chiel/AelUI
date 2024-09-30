cf071fbb4633b412e6df6546e39c15dad3151642
Chiel Kunkels <chiel@kunkels.me>
2024-09-30 21:05:16 +0200

    fix(anchors): set minimum widths

--- AelUI/core/anchors/main.lua
+++ AelUI/core/anchors/main.lua
@@ -2,11 +2,13 @@ local _, ns = ...
 
 local primary = ns.anchors.create('AelUIPrimaryAnchor')
 primary:SetPoint('TOP', UIParent, 'CENTER', 0, -240)
-primary:SetSize((7 * 40) + (6 * 2), 40)
+primary:SetMinWidth((7 * 40) + (6 * 2))
+primary:SetHeight(40)
 
 local secondary = ns.anchors.create('AelUISecondaryAnchor')
 secondary:SetPoint('TOP', primary, 'BOTTOM', 0, -2)
-secondary:SetSize((7 * 34) + (6 * 2), 34)
+secondary:SetMinWidth((7 * 34) + (6 * 2))
+secondary:SetHeight(34)
 
 ns.anchors.primary = primary
 ns.anchors.secondary = secondary
