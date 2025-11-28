4cff11ef5fdfb000058368001499618896c02c70
Chiel Kunkels <chiel@kunkels.me>
2025-11-28 23:12:07 +0000

    feat: add primary + secondary anchors

---
+++ AelUI/core/anchors/create.lua
@@ -0,0 +1,56 @@
+local _, ns = ...
+
+ns.anchors.create = function(name)
+	local minWidth = -1
+	local currentWidth = -1
+	local requestedWidth = -1
+	local resizeListeners = {}
+
+	local anchor = CreateFrame('Frame', name, UIParent)
+
+	local function update()
+		if UnitAffectingCombat('player') then
+			return
+		end
+
+		local newWidth = requestedWidth
+
+		if minWidth > -1 and newWidth < minWidth then
+			newWidth = minWidth
+		end
+
+		if newWidth == currentWidth then
+			return
+		end
+
+		anchor:SetWidth(newWidth)
+		currentWidth = newWidth
+
+		for _, listener in ipairs(resizeListeners) do
+			listener(newWidth)
+		end
+	end
+
+	anchor:SetScript('OnEvent', update)
+	anchor:RegisterEvent('PLAYER_REGEN_ENABLED')
+
+	anchor.OnResize = function(self, listener)
+		table.insert(resizeListeners, listener)
+
+		if currentWidth > -1 then
+			listener(currentWidth)
+		end
+	end
+
+	anchor.SetMinWidth = function(self, width)
+		minWidth = width
+		update()
+	end
+
+	anchor.UpdateWidth = function(self, width)
+		requestedWidth = width
+		update()
+	end
+
+	return anchor
+end
---
+++ AelUI/core/anchors/main.lua
@@ -0,0 +1,14 @@
+local _, ns = ...
+
+local primary = ns.anchors.create('AelUIPrimaryAnchor')
+primary:SetPoint('TOP', UIParent, 'CENTER', 0, -80)
+primary:SetMinWidth((6 * 40) + (5 * 2))
+primary:SetHeight(40)
+
+local secondary = ns.anchors.create('AelUISecondaryAnchor')
+secondary:SetPoint('TOP', primary, 'BOTTOM', 0, -2)
+secondary:SetMinWidth((7 * 34) + (6 * 2))
+secondary:SetHeight(34)
+
+ns.anchors.primary = primary
+ns.anchors.secondary = secondary
