b037c4fb0eef0e6811d12c4cf4f7fa7992ee6f31
Chiel Kunkels <chiel@kunkels.me>
2025-12-15 02:26:00 +0100

    feat(libwa): add animations namespace on icons

---
+++ AelUI/libs/LibWA/animations/animations.lua
@@ -0,0 +1,15 @@
+local _, ns = ...
+
+ns.animations = {}
+
+ns.animations.Create = function()
+	local aura = {}
+
+	aura.Serialize = function(self)
+		local r = {}
+
+		return r
+	end
+
+	return aura
+end
--- AelUI/libs/LibWA/auras/CreateIcon.lua
+++ AelUI/libs/LibWA/auras/CreateIcon.lua
@@ -3,6 +3,7 @@ local _, ns = ...
 LibWA.CreateIcon = function(id)
 	local aura = {
 		base = ns.auras.CreateBase(id),
+		animations = ns.animations.Create(),
 		conditions = ns.conditions.Create(),
 		display = ns.display.CreateIconOptions(),
 		triggers = ns.triggers.Create(),
@@ -15,7 +16,14 @@ LibWA.CreateIcon = function(id)
 	aura.Serialize = function(self)
 		local r = self.base:Serialize()
 		r.regionType = 'icon'
-		return Mixin(r, self.triggers:Serialize(), self.display:Serialize(), self.conditions:Serialize())
+
+		return Mixin(
+			r,
+			self.triggers:Serialize(),
+			self.display:Serialize(),
+			self.conditions:Serialize(),
+			self.animations:Serialize()
+		)
 	end
 
 	return aura
