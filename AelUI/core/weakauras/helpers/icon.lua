8d0ce0c39efac3d8ded090beb43df5c65db4f5e5
Chiel Kunkels <chiel@kunkels.me>
2025-12-16 23:05:59 +0100

    feat(weakauras): add default icon styling

--- AelUI/core/weakauras/helpers/icon.lua
+++ AelUI/core/weakauras/helpers/icon.lua
@@ -1,5 +1,6 @@
 local _, ns = ...
 
+local m = ns.media
 local wa = ns.weakauras
 
 wa.helpers.icon = function(id, config)
@@ -7,6 +8,14 @@ wa.helpers.icon = function(id, config)
 
 	local icon = LibWA.CreateIcon(id)
 	icon.display:SetSize(c.width or 40, c.height or 40)
+	icon.display:SetKeepAspectRatio(true)
+
+	icon.display:AddBorder({
+		color = { 0, 0, 0, 1 },
+		edge = m.borders.default.name,
+		offset = 0,
+		size = 1,
+	})
 
 	return icon
 end
