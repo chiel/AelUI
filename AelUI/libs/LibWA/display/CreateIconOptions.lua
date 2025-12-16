26c91b0dfb80c2ec215330b66c94e97f57cfba58
Chiel Kunkels <chiel@kunkels.me>
2025-12-16 20:21:51 +0100

    feat(libwa): add display method to set icon size

--- AelUI/libs/LibWA/display/CreateIconOptions.lua
+++ AelUI/libs/LibWA/display/CreateIconOptions.lua
@@ -7,11 +7,20 @@ ns.display.CreateIconOptions = function()
 		},
 	}
 
+	aura.SetSize = function(self, width, height)
+		self.size = { width = width, height = height }
+	end
+
 	aura.Serialize = function(self)
 		local r = {
 			cooldown = false,
 			subRegions = {},
 		}
+
+		if self.size ~= nil then
+			r.width = self.size.width
+			r.height = self.size.height
+		end
 
 		return r
 	end
