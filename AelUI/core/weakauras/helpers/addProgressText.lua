ac70aaa21e06f40f66499a6bf8c5b3a6a0b31031
Chiel Kunkels <chiel@kunkels.me>
2025-12-23 00:28:26 +0100

    feat(weakauras/helpers): add addProgressText

---
+++ AelUI/core/weakauras/helpers/addProgressText.lua
@@ -0,0 +1,20 @@
+local _, ns = ...
+
+local wa = ns.weakauras
+local m = ns.media
+
+wa.helpers.addProgressText = function(icon)
+	icon.display:AddText('%p', {
+		font = m.fonts.bold.name,
+		size = 10,
+		justify = 'CENTER',
+		anchor = {
+			from = 'BOTTOM',
+			to = 'BOTTOM',
+			y = -1,
+		},
+		format = {
+			dynamic_threshold = 0,
+		},
+	})
+end
