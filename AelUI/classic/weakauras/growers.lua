f18e480fad260d37f42b5fbdc5712d8b65c55a0d
Chiel Kunkels <chiel@kunkels.me>
2025-12-24 00:38:24 +0100

    fix(weakauras): set group spacing

--- AelUI/classic/weakauras/growers.lua
+++ AelUI/classic/weakauras/growers.lua
@@ -7,9 +7,10 @@ AelUI.weakauras.growers = {}
 local growers = { 'primary' }
 
 for _, name in ipairs(growers) do
+	local gc = wa.config[name].groupConfig
 	local cc = wa.config[name].childConfig
 
-	AelUI.weakauras.growers[name] = wa.createGrower(cc.width, cc.height, 2, function(width)
+	AelUI.weakauras.growers[name] = wa.createGrower(cc.width, cc.height, gc.spacing, function(width)
 		anchors[name]:UpdateWidth(width)
 	end)
 end
