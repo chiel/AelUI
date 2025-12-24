20ab51571cf75a4d12303212644c8f2cc4dc607c
Chiel Kunkels <chiel@kunkels.me>
2025-12-24 01:05:57 +0100

    feat(core): add config

---
+++ AelUI/core/config.lua
@@ -0,0 +1,19 @@
+local _, ns = ...
+
+ns.config = {
+	anchorOffset = 120,
+	groups = {
+		primary = {
+			width = 32,
+			height = 32,
+			spacing = 1,
+			minIcons = 6,
+		},
+		secondary = {
+			width = 28,
+			height = 28,
+			spacing = 1,
+			minIcons = 7,
+		},
+	},
+}
