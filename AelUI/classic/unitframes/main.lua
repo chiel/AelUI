9d23fa8cc16527b0951bc32bbf6cf0cd08fcae11
Chiel Kunkels <chiel@kunkels.me>
2025-12-25 00:15:36 +0100

    fix(unitframes): move frames away from icons

--- AelUI/classic/unitframes/main.lua
+++ AelUI/classic/unitframes/main.lua
@@ -2,8 +2,8 @@ local _, ns = ...
 
 ns.OnEnable(function()
 	PlayerFrame:ClearAllPoints()
-	PlayerFrame:SetPoint('RIGHT', AelUIPrimaryAnchor, 'LEFT', 0, 0)
+	PlayerFrame:SetPoint('RIGHT', AelUIPrimaryAnchor, 'LEFT', -20, 0)
 
 	TargetFrame:ClearAllPoints()
-	TargetFrame:SetPoint('LEFT', AelUIPrimaryAnchor, 'RIGHT', 0, 0)
+	TargetFrame:SetPoint('LEFT', AelUIPrimaryAnchor, 'RIGHT', 20, 0)
 end)
