a8550f037edd2390a89677c19e1df3a4e6ab4ff3
Chiel Kunkels <chiel@kunkels.me>
2025-12-24 01:15:33 +0100

    fix(anchors): use config sizes

--- AelUI/core/anchors/main.lua
+++ AelUI/core/anchors/main.lua
@@ -1,14 +1,17 @@
 local _, ns = ...
 
+local pc = ns.config.groups.primary
+local sc = ns.config.groups.secondary
+
 local primary = ns.anchors.create('AelUIPrimaryAnchor')
 primary:SetPoint('TOP', UIParent, 'CENTER', 0, -120)
-primary:SetMinWidth((6 * 40) + (5 * 2))
-primary:SetHeight(40)
+primary:SetMinWidth((pc.minIcons * pc.width) + ((pc.minIcons - 1) * pc.spacing))
+primary:SetHeight(pc.height)
 
 local secondary = ns.anchors.create('AelUISecondaryAnchor')
-secondary:SetPoint('TOP', primary, 'BOTTOM', 0, -2)
-secondary:SetMinWidth((7 * 34) + (6 * 2))
-secondary:SetHeight(34)
+secondary:SetPoint('TOP', primary, 'BOTTOM', 0, -sc.spacing)
+secondary:SetMinWidth((sc.minIcons * sc.width) + ((sc.minIcons - 1) * sc.spacing))
+secondary:SetHeight(sc.height)
 
 ns.anchors.primary = primary
 ns.anchors.secondary = secondary
