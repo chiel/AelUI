d3e5a9d4d2128bb4ada07c5fc826dae6e050cddf
Chiel Kunkels <chiel@kunkels.me>
2025-12-27 18:23:27 +0100

    fix(config): adjust icon spacing

--- AelUI/core/config.lua
+++ AelUI/core/config.lua
@@ -6,13 +6,13 @@ ns.config = {
 		primary = {
 			width = 32,
 			height = 32,
-			spacing = 1,
+			spacing = 2,
 			minIcons = 6,
 		},
 		secondary = {
 			width = 24,
 			height = 24,
-			spacing = 1,
+			spacing = 2,
 			minIcons = 7,
 		},
 	},
