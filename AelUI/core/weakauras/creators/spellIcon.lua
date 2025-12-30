dac08773218ca2a73c03579bc79226a63b141d6c
Chiel Kunkels <chiel@kunkels.me>
2025-12-31 00:48:17 +0100

    fix(weakauras/creators): add range indicator to spell icons

--- AelUI/core/weakauras/creators/spellIcon.lua
+++ AelUI/core/weakauras/creators/spellIcon.lua
@@ -5,5 +5,12 @@ local c = wa.creators
 local h = wa.helpers
 
 wa.creators.spellIcon = function(spellId)
-	return c.composeAura(c.spellIconBase(spellId), h.addProgressText)
+	return c.composeAura(c.spellIconBase(spellId), h.addProgressText, function(icon)
+		local border = icon.display:Get(2)
+		local trigger = icon.triggers:Get(1)
+
+		local cond = icon.conditions:Add()
+		cond:CheckSpellInRange(trigger, false)
+		cond:ChangeBorderColor(border, { 1, 0, 0, 0.75 })
+	end)
 end
