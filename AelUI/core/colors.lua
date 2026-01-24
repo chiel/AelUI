28f9c7044627dfeea8f09171e09ea9473ec188cc
Chiel Kunkels <chiel@kunkels.me>
2026-01-24 20:13:56 +0100

    fix(colors): override some power colors

---
+++ AelUI/core/colors.lua
@@ -0,0 +1,3 @@
+PowerBarColor[0] = { r = 75 / 255, g = 153 / 255, b = 229 / 255 }
+PowerBarColor[1] = { r = 255 / 255, g = 91 / 255, b = 79 / 255 }
+PowerBarColor[3] = { r = 188 / 255, g = 175 / 255, b = 96 / 255 }
