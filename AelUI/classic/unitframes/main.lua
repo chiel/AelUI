0f573de18e0ec210f19d6139adf3e06578a40bb4
Chiel Kunkels <chiel@kunkels.me>
2026-01-26 22:30:14 +0100

    fix(uf): don't position default blizz frames

--- AelUI/classic/unitframes/main.lua
+++
@@ -1,9 +0,0 @@
-local _, ns = ...
-
-ns.OnEnable(function()
-	PlayerFrame:ClearAllPoints()
-	PlayerFrame:SetPoint('RIGHT', AelUIPrimaryAnchor, 'LEFT', -20, -100)
-
-	TargetFrame:ClearAllPoints()
-	TargetFrame:SetPoint('LEFT', AelUIPrimaryAnchor, 'RIGHT', 20, -100)
-end)
