8b2d49fee20eec36c0ae5209560d883f805ef506
Chiel Kunkels <chiel@kunkels.me>
2026-03-13 01:18:01 +0100

    fix(core/xpbar): add spark

--- AelUI/core/xpbar/render.lua
+++ AelUI/core/xpbar/render.lua
@@ -3,7 +3,7 @@ local _, ns = ...
 local h = ns.helpers
 local m = ns.media
 
-local bar, bd, bg = h.createStatusBar(AelUIParent)
+local bar, bd, bg = h.createStatusBar(AelUIParent, { spark = { overflow = 12 } })
 bd:SetSize(500, 18)
 bd:SetPoint('BOTTOM', AelUIParent, 'BOTTOM', 0, 16)
 
