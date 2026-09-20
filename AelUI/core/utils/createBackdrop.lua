bfe312520eb188290910a12124a665a682194dc9
Chiel Kunkels <chiel@kunkels.me>
2026-09-20 19:02:25 +0200

    add initial utils

---
+++ AelUI/core/utils/createBackdrop.lua
@@ -0,0 +1,22 @@
+local _, ns = ...
+
+local m = ns.media
+
+local defaultBgColor = { 50 / 255, 50 / 255, 50 / 255 }
+
+ns.utils.createBackdrop = function(parent, options)
+	local o = options or {}
+	local borderSize = o.borderSize or 1
+
+	local bd = CreateFrame('Frame', nil, parent, 'BackdropTemplate')
+	bd:SetBackdrop({ edgeFile = m.borders.default.file, edgeSize = borderSize })
+	bd:SetBackdropBorderColor(0, 0, 0, 1)
+
+	local bg = bd:CreateTexture(nil, 'BORDER')
+	bg:SetTexture(m.backgrounds.default.file)
+	bg:SetVertexColor(unpack(defaultBgColor))
+	bg:SetPoint('TOPLEFT', borderSize, -borderSize)
+	bg:SetPoint('BOTTOMRIGHT', -borderSize, borderSize)
+
+	return bd, bg
+end
---
+++ AelUI/core/utils/createStatusBar.lua
@@ -0,0 +1,41 @@
+local _, ns = ...
+
+local m = ns.media
+local u = ns.utils
+
+ns.utils.createStatusBar = function(parent, options)
+	local o = options or {}
+
+	local bd, bg = u.createBackdrop(parent)
+
+	local bar = CreateFrame('StatusBar', nil, bd)
+	bar:SetStatusBarTexture(m.statusbars.default.file)
+	bar:SetClipsChildren(true)
+	bar:SetPoint('TOPLEFT', 1, -1)
+	bar:SetPoint('BOTTOMRIGHT', -1, 1)
+
+	bg:ClearAllPoints()
+
+	if o.orientation == 'VERTICAL' then
+		bar:SetOrientation('VERTICAL')
+		bg:SetPoint('TOPLEFT', bd, 'TOPLEFT', 1, -1)
+		bg:SetPoint('BOTTOMRIGHT', bar:GetStatusBarTexture(), 'TOPRIGHT', 0, 0)
+	else
+		bg:SetPoint('TOPLEFT', bar:GetStatusBarTexture(), 'TOPRIGHT', 0, 0)
+		bg:SetPoint('BOTTOMRIGHT', bd, 'BOTTOMRIGHT', -1, 1)
+	end
+
+	local spark
+	if o.spark then
+		local sparkOpts = type(o.spark) == 'table' and o.spark or {}
+		spark = bar:CreateTexture(nil, 'OVERLAY')
+		spark:SetTexture([[Interface\CastingBar\UI-CastingBar-Spark]])
+		spark:SetBlendMode('ADD')
+		spark:SetWidth(10)
+		local overflow = sparkOpts.overflow or 4
+		spark:SetPoint('TOP', bar:GetStatusBarTexture(), 'TOPRIGHT', 0, overflow)
+		spark:SetPoint('BOTTOM', bar:GetStatusBarTexture(), 'BOTTOMRIGHT', 0, -overflow)
+	end
+
+	return bar, bd, bg, spark
+end
