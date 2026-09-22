977eb5465b35fd8fb831d87f784607604cd11c0a
Chiel Kunkels <chiel@kunkels.me>
2026-09-22 18:16:39 +0200

    wip

--- AelUI/core/anchors/main.lua
+++ AelUI/core/anchors/main.lua
@@ -1,8 +1,19 @@
 local _, ns = ...
 
+-- local pc = ns.config.groups.primary
+-- local sc = ns.config.groups.secondary
+
 local primary = ns.anchors.create('AelUIPrimaryAnchor')
 primary:SetPoint('TOP', AelUIParent, 'CENTER', 0, -280)
 primary:SetMinWidth(358)
 primary:SetHeight(58)
+-- primary:SetMinWidth((pc.minIcons * pc.width) + ((pc.minIcons - 1) * pc.spacing))
+-- primary:SetHeight(pc.height)
+
+-- local secondary = ns.anchors.create('AelUISecondaryAnchor')
+-- secondary:SetPoint('TOP', primary, 'BOTTOM', 0, -sc.spacing)
+-- secondary:SetMinWidth((sc.minIcons * sc.width) + ((sc.minIcons - 1) * sc.spacing))
+-- secondary:SetHeight(sc.height)
 
 ns.anchors.primary = primary
+-- ns.anchors.secondary = secondary
--- AelUI/forever/unitframes/units/spawn.lua
+++ AelUI/forever/unitframes/units/spawn.lua
@@ -57,6 +57,30 @@ ns.unitframes.units.spawn = function(unit, fn)
 				self:Update()
 			end,
 		}
+	elseif unit == 'targettarget' then
+		-- f:RegisterEvent('PLAYER_TARGET_CHANGED')
+		-- f.eventCallbacks['PLAYER_TARGET_CHANGED'] = {
+		-- 	function(self)
+		-- 		self:Update()
+		-- 	end,
+		-- }
+		-- f:RegisterEvent('UNIT_TARGET')
+		-- f.eventCallbacks['UNIT_TARGET'] = {
+		-- 	function(self, event, unit)
+		-- 		if unit == 'target' then
+		-- 			self:Update()
+		-- 		end
+		-- 	end,
+		-- }
+	elseif unit == 'pet' then
+		-- f:RegisterEvent('UNIT_PET')
+		-- f.eventCallbacks['UNIT_PET'] = {
+		-- 	function(self, event, unit)
+		-- 		if unit == 'player' then
+		-- 			self:Update()
+		-- 		end
+		-- 	end,
+		-- }
 	end
 
 	fn(f)
